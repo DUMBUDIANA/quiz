daf3b076565053476f4e44bcfc663b24d70eb153
DUMBUDIANA <diana.duumbu@gmail.com>
2024-06-13 12:32:55 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage.js';
-// import quizData from './components/quizData.js';
+
 
 import './App.css';
 import QuizPage from './components/QuizPage.js';
